6ec877b7b9cd0dc9c35e8f4137dfd1193b230d03
dawnlh <z_zhi_hong@163.com>
2025-06-02 10:55:19 +0000

    Deploying to gh-pages from @ zhihongz/zhihongz_cn@4017a55392a120243aa3b46082052f854919f597 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "Repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "My github accounts and some github projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/zhihongz_cn/repositories/";
